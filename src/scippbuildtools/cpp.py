e48793828e3de1359240f32ce510fb6dd4730af8
Neil Vaytet <neil.vaytet@esss.se>
2021-08-23 21:11:39 +0200

    flake8

--- src/scippbuildtools/cpp.py
+++ src/scippbuildtools/cpp.py
@@ -1,14 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
-# @author Neil Vaytet
-
 import os
 import argparse
 import shutil
 import subprocess
 import multiprocessing
 import sys
-import time
 
 
 def _run_command(cmd, shell):
@@ -25,8 +22,7 @@ def _get_absolute_paths(*paths):
 
 
 def make_argument_parser():
-    parser = argparse.ArgumentParser(
-        description='Build C++ library and run tests')
+    parser = argparse.ArgumentParser(description='Build C++ library and run tests')
     parser.add_argument('--prefix', default='install')
     parser.add_argument('--source_dir', default='.')
     parser.add_argument('--build_dir', default='build')
@@ -35,31 +31,15 @@ def make_argument_parser():
 
 
 class CppBuilder:
-    def __init__(self,
-                 prefix=None,
-                 source_dir=None,
-                 build_dir=None,
-                 caching=None):
+    """
+    Platform-independent builder to run cmake, build, install and C++ tests.
+    """
+    def __init__(self, prefix=None, source_dir=None, build_dir=None, caching=None):
 
         self._prefix, self._build_dir, self._source_dir = _get_absolute_paths(
             prefix, build_dir, source_dir)
         self._caching = caching
-
         self._config = {}
-        # self._flags_list = None
-        # self._build_flags = None
-        # self._shell = None
-        # self._build_config = None
-
-    # def __init__(self,
-    #              flags_list=None,
-    #              build_flags=None,
-    #              shell=None,
-    #              build_config=None):
-    #     self.flags_list = flags_list
-    #     self.build_flags = build_flags
-    #     self.shell = shell
-    #     self.build_config = build_config
 
     def enter_build_dir(self):
         if not os.path.exists(self._build_dir):
@@ -67,17 +47,9 @@ class CppBuilder:
         os.chdir(self._build_dir)
 
     def cmake_configure(self):
-        """
-        Platform-independent function to run cmake, build, install and C++ tests.
-        """
 
         # Get the platform name: 'linux', 'darwin' (osx), or 'win32'.
         self._config["platform"] = sys.platform
-
-        # # Set up absolute directory paths
-        # source_dir = os.path.abspath(source_dir)
-        # prefix = os.path.abspath(prefix)
-        # build_dir = os.path.abspath(build_dir)
 
         # Default options
         self._config["shell"] = False
@@ -104,9 +76,8 @@ class CppBuilder:
                     '-DCMAKE_OSX_DEPLOYMENT_TARGET':
                     osxversion,
                     '-DCMAKE_OSX_SYSROOT':
-                    os.path.join('/Applications', 'Xcode.app', 'Contents',
-                                 'Developer', 'Platforms', 'MacOSX.platform',
-                                 'Developer', 'SDKs',
+                    os.path.join('/Applications', 'Xcode.app', 'Contents', 'Developer',
+                                 'Platforms', 'MacOSX.platform', 'Developer', 'SDKs',
                                  'MacOSX{}.sdk'.format(osxversion))
                 })
 
@@ -114,8 +85,7 @@ class CppBuilder:
             cmake_flags.update({'-G': 'Visual Studio 16 2019', '-A': 'x64'})
             # clcache conda installed to env Scripts dir in env if present
             scripts = os.path.join(os.environ.get('CONDA_PREFIX'), 'Scripts')
-            if self._caching and os.path.exists(
-                    os.path.join(scripts, 'clcache.exe')):
+            if self._caching and os.path.exists(os.path.join(scripts, 'clcache.exe')):
                 cmake_flags.update({'-DCLCACHE_PATH': scripts})
             self._config["shell"] = True
             self._config["build_config"] = 'Release'
@@ -128,9 +98,7 @@ class CppBuilder:
             self._config["build_flags"] = [parallel_flag]
 
         if len(self._config["build_config"]) > 0:
-            self._config["build_flags"] += [
-                '--config', self._config["build_config"]
-            ]
+            self._config["build_flags"] += ['--config', self._config["build_config"]]
 
         # Parse cmake flags
         self._config["flags_list"] = []
@@ -142,8 +110,7 @@ class CppBuilder:
 
     def cmake_run(self):
         # Run cmake
-        _run_command(['cmake'] + self._config["flags_list"] +
-                     [self._source_dir],
+        _run_command(['cmake'] + self._config["flags_list"] + [self._source_dir],
                      shell=self._config["shell"])
         # Show cmake settings
         _run_command(['cmake', '-B', '.', '-S', self._source_dir, '-LA'],
@@ -152,23 +119,12 @@ class CppBuilder:
     def cmake_build(self, target_list):
         # Compile benchmarks, C++ tests, and python library
         for target in target_list:
-            run_command(['cmake', '--build', '.', '--target', target] +
-                        self._config["build_flags"],
-                        shell=self._config["shell"])
+            _run_command(['cmake', '--build', '.', '--target', target] +
+                         self._config["build_flags"],
+                         shell=self._config["shell"])
 
-    def run_cpp_tests(test_list):
+    def run_cpp_tests(self, test_list):
 
         for test in test_list:
-            run_command(
-                [os.path.join('bin', self._config["build_config"], test)],
-                shell=self._config["shell"])
-        # # Run C++ tests
-        # run_command([os.path.join('bin', build_config, 'scippneutron-test')],
-        #             shell=shell)
-
-    # if __name__ == '__main__':
-    #     args = parser.parse_args()
-    #     main(prefix=args.prefix,
-    #          build_dir=args.build_dir,
-    #          source_dir=args.source_dir,
-    #          caching=args.caching)
+            _run_command([os.path.join('bin', self._config["build_config"], test)],
+                         shell=self._config["shell"])
