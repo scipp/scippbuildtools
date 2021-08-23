0a804ed063c84d769b1a612e9375517a791e701d
Neil Vaytet <neil.vaytet@esss.se>
2021-08-23 15:14:30 +0200

    add conda meta dn setup files

---
+++ setup.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
+
+import setuptools
+
+setuptools.setup(name='scippbuildtools',
+                 packages=setuptools.find_packages('src'),
+                 package_dir={"": "src"})
